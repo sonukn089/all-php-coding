32a56421e2f57bbbf10ee7d4941e2c0f1012947b
Sonu <sonukn089@gmail.com>
2018-05-05 23:49:51 +0530

    form is ready

--- process.php
+++ process.php
@@ -1,5 +1,9 @@
 <?php
 
-
+	echo "Name: ".$_POST['name']."<br>";
+	echo "Email: ".$_POST['email']."<br>";
+	echo "Address: ".$_POST['address']."<br>";
+	echo "Password: ".$_POST['password']."<br>";
+	echo "CPassword: ".$_POST['cpassword'];
 
 ?>
