7cb0690128014ad31e4ff88b4d9cc8c39ed4a751
Sonu <sonukn089@gmail.com>
2018-05-08 22:46:15 +0530

    add something

--- Information.php
+++ Information.php
@@ -1,3 +1,5 @@
+
+<h1 style="background-color: #ff8000;">Student Information</h1>
 <?php
 
 	echo "Scholar Number : ".$_POST['scholar_no']."<br>";
@@ -5,62 +7,86 @@
 	echo "Middile Name : ".$_POST['middle_name']."<br>";
 	echo "Last Name : ".$_POST['scholar_no']."<br>";
 	echo "<hr>";
+?>	
+<?php
 	echo "Date of birth : ".$_POST['bday']."<br>";
 	echo "Gender : ".$_POST['gender']."<br>";
 	echo "Blood Group : ".$_POST['middle_name']."<br>";
 	echo "Birth Place : ".$_POST['scholar_no'];
 	echo "<hr>";
-	echo "Mother Tongue : ".$_POST['mother_tongue'];
-	echo "Category : ".$_POST['category'];
-	echo "Religion : ".$_POST['religion'];
-	echo "Address Line 1 :".$_POST['address1'];
-	echo "<hr>";
-	echo $_POST['address2'];
-	echo $_POST['city'];
-	echo $_POST['state'];
-	echo $_POST['pin'];
-	echo "<hr>";
-	echo $_POST['country'];
-	echo $_POST['phone'];
-	echo $_POST['mobile'];
-	echo $_POST['email'];
-	echo "<hr>";
-	echo $_POST['fname'];
-	echo $_POST['mname'];
-	echo $_POST['caretaker'];
-	echo $_POST['relation'];
-	echo "<hr>";
-	echo $_POST['feducation'];
-	echo $_POST['meducation'];
-	echo $_POST['foccupation'];
-	echo $_POST['moccupation'];
-	echo "<hr>";
-	echo $_POST['fai'];
-	echo $_POST['fadd'];
-	echo $_POST['fcity'];
-	echo $_POST['fstate'];
-	echo "<hr>";
-	echo $_POST['fpin'];
-	echo $_POST['fcountry'];
-	echo $_POST['fphone'];
-	echo $_POST['fmobile'];
-	echo "<hr>";
-	echo $_POST['mmobile'];
-	echo $_POST['transport'];
-	echo $_POST['password'];
-	echo $_POST['cpassword'];
-	echo "<hr>";
-	echo $_POST['adate'];
-	echo $_POST['class'];
-	echo $_POST['section'];
-	echo $_POST['stream'];
-	echo "<hr>";
-	echo $_POST['pclass'];
-	echo $_POST['pyear'];
-	echo $_POST['sname'];
-	echo $_POST['roll'];
-	echo $_POST['percentage'];
-	echo $_POST['subject'];
-
-
 ?>
+<?php
+	echo "Mother Tongue : ".$_POST['mother_tongue']."<br>";
+	echo "Category : ".$_POST['category']."<br>";
+	echo "Religion : ".$_POST['religion']."<br>";
+	echo "Address Line 1 :".$_POST['address1']."<br>";
+	echo "<hr>";
+?>
+<?php
+	echo "Address Line 2 : ".$_POST['address2']."<br>";
+	echo "City : ".$_POST['city']."<br>";
+	echo "State : ".$_POST['state']."<br>";
+	echo "pin".$_POST['pin']."<br>";
+	echo "<hr>";
+?>
+<?php
+	echo "Country : ".$_POST['country']."<br>";
+	echo "Phone No : ".$_POST['phone']."<br>";
+	echo $_POST['mobile']."<br>";
+	echo $_POST['email']."<br>";
+	echo "<hr>";
+?>
+<h1 style="background-color: #ff8000;">Parrent Information</h1>
+<?php
+	echo $_POST['fname']."<br>";
+	echo $_POST['mname']."<br>";
+	echo $_POST['caretaker']."<br>";
+	echo $_POST['relation']."<br>";
+	echo "<hr>";
+?>
+<?php
+	echo $_POST['feducation']."<br>";
+	echo $_POST['meducation']."<br>";
+	echo $_POST['foccupation']."<br>";
+	echo $_POST['moccupation']."<br>";
+	echo "<hr>";
+?>
+<?php
+	echo $_POST['fai']."<br>";
+	echo $_POST['fadd']."<br>";
+	echo $_POST['fcity']."<br>";
+	echo $_POST['fstate']."<br>";
+	echo "<hr>";
+?>
+<?php
+	echo $_POST['fpin']."<br>";
+	echo $_POST['fcountry']."<br>";
+	echo $_POST['fphone']."<br>";
+	echo $_POST['fmobile']."<br>";
+	echo "<hr>";
+?>
+<?php
+	echo $_POST['mmobile']."<br>";
+	echo $_POST['transport']."<br>";
+	echo $_POST['password']."<br>";
+	echo $_POST['cpassword']."<br>";
+	echo "<hr>";
+?>
+<h1 style="background-color: #ff8000;">School Information</h1>
+<?php
+	echo $_POST['adate']."<br>";
+	echo $_POST['class']."<br>";
+	echo $_POST['section']."<br>";
+	echo $_POST['stream']."<br>";
+	echo "<hr>";
+?>
+
+<?php
+	echo $_POST['pclass']."<br>";
+	echo $_POST['pyear']."<br>";
+	echo $_POST['sname']."<br>";
+	echo $_POST['roll']."<br>";
+	echo $_POST['percentage']."<br>";
+	echo $_POST['subject']."<br>";
+?>
+
