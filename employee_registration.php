9438db7985598a858f9292edc424701be739760a
Sonu <sonukn089@gmail.com>
2018-05-24 22:43:19 +0530

    add something

--- employee_registration.php
+++ employee_registration.php
@@ -7,7 +7,7 @@
 	<center>
 
 		<h1 style="background: red; color: white;">Employee Registration Form</h1>
-		
+
 		<form method="POST" action="employee_data.php">
 			<table>
 				<tr>
@@ -79,6 +79,7 @@
 				</tr>
 			</table>
 			<h1 style="background: red; color: white;">Education Details and Experience</h1>
+			
 			<table>
 				<tr>
 					<td>Graduation</td>
